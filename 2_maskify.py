1688df145e7b4de35da937cdec075071cbeb2c79
UsmanShaiik <136417770+UsmanShaiik@users.noreply.github.com>
2024-04-17 23:20:48 +0530

    Update 2_maskify.py

--- 2_maskify.py
+++ 2_maskify.py
@@ -1,3 +1,10 @@
+""" Maskify Function (maskify):
+Input Validation:
+Check if the mobile number provided is long enough to maskify.
+
+Masking Process:
+Replace all but the last three characters of the mobile number with "#" to mask it.
+Output the masked number. """
 def maskify(mobile_number):
     if len(mobile_number) < 3:
         print("Mobile number is too short to maskify.")
